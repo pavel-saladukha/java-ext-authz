2f9d5553b4bfde5427bdbb48279e5f127f955198
Pavel Saladukha <pavel.soloduha@outlook.com>
2023-11-15 21:49:16 +0100

    1

--- src/main/java/de/dkwr/authzexample/AuthorizationServiceImpl.java
+++ src/main/java/de/dkwr/authzexample/AuthorizationServiceImpl.java
@@ -2,8 +2,11 @@ package de.dkwr.authzexample;
 
 import com.google.rpc.Code;
 import com.google.rpc.Status;
+import io.envoyproxy.envoy.config.core.v3.HeaderValue;
+import io.envoyproxy.envoy.config.core.v3.HeaderValueOption;
 import io.envoyproxy.envoy.service.auth.v3.AuthorizationGrpc;
 import io.envoyproxy.envoy.service.auth.v3.CheckResponse;
+import io.envoyproxy.envoy.service.auth.v3.OkHttpResponse;
 
 import java.util.Map;
 import java.util.Objects;
@@ -18,13 +21,17 @@ public class AuthorizationServiceImpl extends AuthorizationGrpc.AuthorizationImp
                       io.grpc.stub.StreamObserver<io.envoyproxy.envoy.service.auth.v3.CheckResponse> done) {
         Map<String, String> headers = request.getAttributes().getRequest().getHttp().getHeadersMap();
 
-        if (!Objects.equals(headers.get("x-ext-authz"), "allow")) {
+        System.out.println("header value = " + headers.get("x-ext-authz"));
+        System.out.println("if value " + Objects.equals(headers.get("x-ext-authz"), "allow"));
+
+        if (Objects.equals(headers.get("x-ext-authz"), "allow") == Boolean.FALSE) {
             done.onNext(
                     CheckResponse
                             .newBuilder()
                             .setStatus(Status.newBuilder().setCode(Code.UNAUTHENTICATED_VALUE).build())
                             .setDeniedResponse(CheckResponse.getDefaultInstance().getDeniedResponse())
-                            .build());
+                    .build()
+            );
 
             done.onCompleted();
             return;
@@ -33,8 +40,15 @@ public class AuthorizationServiceImpl extends AuthorizationGrpc.AuthorizationImp
         done.onNext(CheckResponse
                 .newBuilder()
                 .setStatus(Status.newBuilder().setCode(Code.OK_VALUE).build())
-                .setOkResponse(CheckResponse.getDefaultInstance().getOkResponse())
+                .setOkResponse(OkHttpResponse.newBuilder()
+                        .addHeaders(
+                                HeaderValueOption.newBuilder().setHeader(
+                                        HeaderValue.newBuilder().setKey("oh-my-gud").setValue("valsiruem").build()
+                                ).build()
+                        ).build())
                 .build());
+
         done.onCompleted();
+
     }
 }
